1c11d749dd0a180d84b09086a2d8f84eceb67cf5
lizzy-km <oxygenisme.02@gmail.com>
2023-11-02 20:29:34 +0630

    add some animation

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -16,12 +16,9 @@ const Home = () => {
   return (
     <div className=" relative items-center  flex w-full h-auto bg-[#d8c7af] flex-col ">
       <div className=" w-full h-auto flex flex-col justify-center items-center ">
-        <div className=" m-[2rem] w-[80%] h-[280px] bg-white "></div>
-        <div className=" m-[2rem] w-[80%] h-[280px] bg-white "></div>
-        <div className=" m-[2rem] w-[80%] h-[280px] bg-white "></div>
-        <div className=" m-[2rem] w-[80%] h-[280px] bg-white "></div>
-        <div className=" m-[2rem] w-[80%] h-[280px] bg-white "></div>
-        <div className=" m-[2rem] w-[80%] h-[280px] bg-white "></div>
+        <div className=" card m-[2rem]  h-[280px] bg-white "></div>
+        <div className=" card m-[2rem]  h-[280px] bg-white "></div>
+        
       </div>
     </div>
   );
