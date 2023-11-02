5fc26f9bc5fa5a9d74a7de21a23f2533a488902e
lizzy-km <oxygenisme.02@gmail.com>
2023-11-02 16:52:29 +0630

    Add CategoryData

---
+++ src/components/Header/CategoryData.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react'
+import { commerce } from '../../lib/commerce';
+
+const CategoryData = () => {
+    const [category, setCategory] = useState([]);
+
+  const fetchProducts = async () => {
+    const response = await commerce.categories.list();
+    setCategory((response && response.data) || []);
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  return {category}
+  
+}
+
+export default CategoryData
