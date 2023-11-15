4900b2202e3d3f45aaab19908106322ac8d6f1cc
lizzy-km <oxygenisme.02@gmail.com>
2023-11-15 21:18:23 +0630

    runOnce Test

--- src/App.jsx
+++ src/App.jsx
@@ -41,6 +41,7 @@ const App = () => {
 
   const [currentPosition, setCurrentPosition] = useState(0);
   const [topPosition, setTopPosition] = useState(15);
+  const box = document.getElementById("box");
 
   const useHandleScroll = (e) => {
     setOffset(e.target.scrollTop);
@@ -69,9 +70,39 @@ const App = () => {
 
     const clampedTopPosition = Math.min(90, Math.max(15, newTopPosition));
 
-    setCurrentPosition(clampedPosition);
     setTopPosition(clampedTopPosition);
   };
+
+  useEffect(() => {
+    main?.scrollTop > 100 ? setCurrentPosition(1) : setCurrentPosition(0);
+  }, [main?.scrollTop]);
+
+  const boxWidth = box?.offsetWidth;
+  const width = topPosition / 30;
+
+  function runOnce(func, context) {
+    let ran;
+    return function () {
+      if (func) {
+        ran = func.apply(context || this, arguments);
+        func = null;
+      }
+      return ran;
+    };
+  }
+
+  const value = 5
+  const sec =8
+
+  const test = runOnce((a,b) => console.log('Testing', a,b) )
+
+  useEffect(()=>{
+    test( 'one_ ' +  main?.scrollTop,topPosition)
+  test( 'two_ ' + main?.scrollTop,topPosition)
+  test( 'three_ ' + main?.scrollTop,topPosition)
+  },[main?.scrollTop,topPosition])
+
+  
 
   const { mobile, tablet, desktop } = useResponsive();
 
@@ -83,38 +114,50 @@ const App = () => {
         className=" transition-all pt-[140px] bg-[#d8c7af] relative w-full max-h-screen overflow-y-auto flex flex-col "
       >
         <div
+          style={{
+            opacity: currentPosition,
+          }}
+          onClick={() => main.scrollTo(0, 0)}
+          className=" active:p-2 hover:p-1 w-[50px] h-[50px] flex justify-center items-center rounded-full z-[999999] cursor-pointer bottom-10 left-10 fixed p-2 bg-black text-[#d8c7af] "
+        >
+          <p className=" w-full text-center h-full bg-[#212121] rounded-full ">
+            Up
+          </p>
+        </div>
+        <div
           id="box"
           style={{
             top: topPosition + "vh",
-            left: currentPosition + "vw",
+            right: 0 + "vw",
+
+            height: boxWidth,
+            width: width + 2 + "%",
           }}
-          className=" rounded-bl-[50px] p-2 rounded-tr-[50px] transition-all z-[9999999] bg-[#d8c7af] fixed h-[100px] w-[100px] "
+          className=" rounded-bl-[50px] p-2 rounded-tr-[50px] transition-all z-[9999999] bg-[#d8c7af] fixed   "
         >
-          <div className=" p-1 rounded-full bg-[#685944] w-full h-full " >
-          <div className=" p-1 rounded-t-full rounded-b-full bg-[#f0eeec] w-full h-full " >
-            
-            </div>
+          <div className=" p-1 hover:p-1 cursor-pointer rounded-full bg-[#685944] w-full h-full ">
+            <div className=" p-1 sh rounded-t-full rounded-b-full bg-[#f0eeec] w-full h-full "></div>
           </div>
         </div>
         <div
           id="box"
           style={{
             top: topPosition + "vh",
-            right: currentPosition + "vw",
+            right: 0 + "vw",
+            width: width + 2 + "%",
+            height: boxWidth,
           }}
-          className=" rounded-br-[50px] p-2 rounded-tl-[50px] transition-all z-[9999999] bg-[#d8c7af] fixed h-[100px] w-[100px] "
+          className=" rounded-[50px] p-2  transition-all z-[9999999] bg-[#d8c7af] fixed   "
         >
-           <div className=" p-1 rounded-full bg-[#685944] w-full h-full " >
-          <div className=" p-1 rounded-t-full rounded-b-full bg-[#f0eeec] w-full h-full " >
-            
-            </div>
+          <div className=" p-1  hover:p-2 cursor-pointer rounded-full bg-[#685944] w-full h-full ">
+            <div className=" p-1 sh rounded-t-full rounded-b-full bg-[#f0eeec] w-full h-full "></div>
           </div>
         </div>
         <div
           id="NavHeight"
           className=" w-full flex h-auto fixed top-0 z-[99999]  "
         >
-          {mobile && _jsx(NavBarM, {})}
+          {mobile && <NavBarM />}
           {desktop && <NavBar />}
           {tablet && <NavBarM />}
         </div>
